9d64fc7a0e4fa67bb1197341d8e48ea0371a50de
Maelle-dsnl <maelle.desnoyelle@esme.fr>
2026-09-25 08:56:25 +0000

    etape 3

--- program.c
+++ program.c
@@ -12,33 +12,41 @@ int main() {
     // Initialisation du générateur aléatoire
     srand(time(NULL));
 
-    printf("=== PIERRE FEUILLE CISEAUX (Étape 2) ===\n");
-    printf("Règles : 1 = Pierre, 2 = Feuille, 3 = Ciseaux\n");
-    printf("Arrêt : 2 points d'écart ou 7 manches maximum.\n\n");
+    printf("=== PIERRE FEUILLE CISEAUX LÉZARD SPOCK ===\n");
+    printf("Règles :\n");
+    printf("1 = Pierre, 2 = Feuille, 3 = Ciseaux, 4 = Lézard, 5 = Spock\n");
+    printf("Arrêt : 2 points d'écart ou 7 manches max.\n\n");
 
-    // Condition de l'étape 2 :
-    // On continue TANT QUE l'écart est strictement inférieur à 2 ET la manche <= 7
+    // Condition d'arrêt : 2 points d'écart OU manche > 7
     while (abs(scoreJoueur - scoreOrdi) < 2 && manche <= 7) {
         printf("--- Manche %d/7 ---\n", manche);
 
         // Saisie du joueur
-        printf("Votre choix (1, 2 ou 3) : ");
+        printf("Votre choix (1, 2, 3, 4 ou 5) : ");
         if (scanf("%d", &choixJoueur) != 1) {
-            // Vider le tampon en cas de saisie invalide
-            while (getchar() != '\n');
+            while (getchar() != '\n'); // Vider le tampon en cas d'erreur
             continue;
         }
 
-        // Choix aléatoire de l'ordinateur (1, 2 ou 3)
-        choixOrdi = (rand() % 3) + 1;
+        // Choix aléatoire de l'ordinateur entre 1 et 5
+        choixOrdi = (rand() % 5) + 1;
         printf("L'ordinateur a choisi : %d\n", choixOrdi);
 
-        // Détermination du gagnant de la manche
+        // Détermination du gagnant
         if (choixJoueur == choixOrdi) {
             printf("Égalité !\n");
-        } else if ((choixJoueur == 1 && choixOrdi == 3) ||
-                   (choixJoueur == 2 && choixOrdi == 1) ||
-                   (choixJoueur == 3 && choixOrdi == 2)) {
+        } 
+        // 10 combinaisons gagnantes pour le joueur :
+        // 1 (Pierre) bat 3 (Ciseaux) et 4 (Lézard)
+        // 2 (Feuille) bat 1 (Pierre) et 5 (Spock)
+        // 3 (Ciseaux) bat 2 (Feuille) et 4 (Lézard)
+        // 4 (Lézard) bat 2 (Feuille) et 5 (Spock)
+        // 5 (Spock) bat 1 (Pierre) et 3 (Ciseaux)
+        else if ((choixJoueur == 1 && (choixOrdi == 3 || choixOrdi == 4)) ||
+                 (choixJoueur == 2 && (choixOrdi == 1 || choixOrdi == 5)) ||
+                 (choixJoueur == 3 && (choixOrdi == 2 || choixOrdi == 4)) ||
+                 (choixJoueur == 4 && (choixOrdi == 2 || choixOrdi == 5)) ||
+                 (choixJoueur == 5 && (choixOrdi == 1 || choixOrdi == 3))) {
             printf("Vous gagnez cette manche !\n");
             scoreJoueur++;
         } else {
